ebae96bd4241cc7910305be707ec6e6268f18ac9
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-21 00:01:43 +0100

    Ajout support UE dans IRepositoryFactory

--- UniversiteDomain/DataAdapters/DataAdaptersFactory/IRepositoryFactory.cs
+++ UniversiteDomain/DataAdapters/DataAdaptersFactory/IRepositoryFactory.cs
@@ -4,6 +4,7 @@
  {
      IParcoursRepository ParcoursRepository();
      IEtudiantRepository EtudiantRepository();
+     IUeRepository UeRepository(); 
      // Méthodes de gestion de la dadasource
      // Ce sont des méthodes qui permettent de gérer l'ensemble des data source
      // comme tout supprimer ou tout créer
