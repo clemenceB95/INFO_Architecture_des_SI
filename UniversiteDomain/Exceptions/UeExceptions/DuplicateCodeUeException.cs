5cf9afc2b40b69a6a194bef3e34ed59d4038472e
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 22:37:18 +0100

    Feat: ajout de DuplicateCodeUeException

---
+++ UniversiteDomain/Exceptions/UeExceptions/DuplicateCodeUeException.cs
@@ -0,0 +1,8 @@
+﻿namespace UniversiteDomain.Exceptions.UeExceptions;
+
+[Serializable]
+public class DuplicateCodeUeException : Exception
+{
+    public DuplicateCodeUeException(string code)
+        : base($"Une UE existe déjà avec le code : {code}") { }
+}
