bd16518341f5d497733308648e41cb0e2e94d086
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 23:11:52 +0100

    Feat: création de l'entité Ue avec propriétés Code et NomUe

---
+++ UniversiteDomain/Entities/Ue.cs
@@ -0,0 +1,8 @@
+﻿namespace UniversiteDomain.Entities;
+
+public class Ue
+{
+    public long Id { get; set; }
+    public string Code { get; set; } = string.Empty;  // Exemple : MATH101
+    public string NomUe { get; set; } = string.Empty; // Exemple : Algorithmique
+}
