ec5c3644a4e860fad8362995862535473903ff1d
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 22:30:41 +0100

    Feat: implémentation du CreateParcoursUseCase avec RepositoryFactory

---
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -0,0 +1,32 @@
+﻿using UniversiteDomain.DataAdapters.DataAdaptersFactory;
+using UniversiteDomain.Entities;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
+
+public class CreateParcoursUseCase
+{
+    private readonly IRepositoryFactory factory;
+
+    public CreateParcoursUseCase(IRepositoryFactory factory)
+    {
+        this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
+    }
+
+    public async Task<Parcours> ExecuteAsync(Parcours parcours)
+    {
+        ArgumentNullException.ThrowIfNull(parcours);
+
+        var repo = factory.ParcoursRepository();
+
+        // Vérifie si le parcours existe déjà
+        var existants = await repo.FindByConditionAsync(p => p.Id == parcours.Id);
+        if (existants != null && existants.Count > 0)
+            throw new Exception("Le parcours existe déjà"); // tu peux mettre une exception perso
+
+        // Création
+        var created = await repo.CreateAsync(parcours);
+
+        await factory.SaveChangesAsync();
+        return created;
+    }
+}
