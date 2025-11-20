1d46770bf792963374ec1a51704b78a8ea1da8e0
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 23:07:42 +0100

    Feat: ajout de l'interface IUeRepository avec méthodes CRUD

--- UniversiteDomain/Entities/Parcours.cs
+++ UniversiteDomain/Entities/Parcours.cs
@@ -7,7 +7,7 @@ public class Parcours
     public int AnneeFormation { get; set; } = 1;
     // OneToMany : un parcours contient plusieurs étudiants
     // Remarque : pour éviter quelques NullPointerException disgracieux, j'ai choisi de créer une liste d'incrits vide quand aucun étudiant n'est inscrit dans un parcours plutôt que de l'initialiser à null
-    public List<Etudiant>? Inscrits { get; set; } = new();
+    public List<Etudiant> Inscrits { get; set; } = new();
 
     public override string ToString()
     {
