7958edeacb3dbb50ede21defa2f8d8f4803c4cc3
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 22:51:12 +0100

    Feat: ajout de InvalidUeNameException

---
+++ UniversiteDomain/Exceptions/UeExceptions/InvalidUeNameException.cs
@@ -0,0 +1,8 @@
+﻿namespace UniversiteDomain.Exceptions.UeExceptions;
+
+[Serializable]
+public class InvalidUeNameException : Exception
+{
+    public InvalidUeNameException() 
+        : base("Le nom d'une UE doit contenir plus de 3 caractères.") { }
+}
