10a7a46e74f690c0eed4c8e2d536fd1d28afb337
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-20 22:58:57 +0100

    Feat: ajout de l'interface IUeRepository avec méthodes CRUD

---
+++ UniversiteDomain/DataAdapters/IUeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using UniversiteDomain.Entities;
+
+namespace UniversiteDomain.DataAdapters;
+
+public interface IUeRepository
+{
+    // Création d’une UE
+    Task<Ue> CreateAsync(Ue ue);
+    
+    // Recherche d’une UE par son Id
+    Task<Ue?> GetByIdAsync(long id);
+    
+    // Recherche conditionnelle (ex: par code)
+    Task<List<Ue>> FindByConditionAsync(Expression<Func<Ue, bool>> predicate);
+    
+    // Récupération de toutes les UEs
+    Task<List<Ue>> GetAllAsync();
+    
+    // Mise à jour
+    Task<Ue> UpdateAsync(Ue ue);
+    
+    // Suppression
+    Task DeleteAsync(long id);
+}
