7eb4f79616f8261b3c5f9a2ea18a9fe20ec96a96
Clémence Boitelle <clemence.boitelle@etud.u-picardie.fr>
2025-11-21 01:46:44 +0100

    refactor: conversion de CreateUeUseCase en primary constructor

--- UniversiteDomain/UseCases/UeUseCases/Create/CreateUeUseCase.cs
+++ UniversiteDomain/UseCases/UeUseCases/Create/CreateUeUseCase.cs
@@ -1,18 +1,12 @@
-﻿using UniversiteDomain.DataAdapters;
+﻿using UniversiteDomain.DataAdapters.DataAdaptersFactory;
 using UniversiteDomain.Entities;
 using UniversiteDomain.Exceptions.UeExceptions;
 
 namespace UniversiteDomain.UseCases.UeUseCases.Create;
 
-public class CreateUeUseCase
+public class CreateUeUseCase(IRepositoryFactory factory)
 {
-    private readonly IUeRepository _ueRepository;
-
-    public CreateUeUseCase(IUeRepository ueRepository)
-    {
-        _ueRepository = ueRepository 
-                        ?? throw new ArgumentNullException(nameof(ueRepository));
-    }
+    private readonly IRepositoryFactory _factory = factory ?? throw new ArgumentNullException(nameof(factory));
 
     public async Task<Ue> ExecuteAsync(Ue ue)
     {
@@ -22,12 +16,19 @@ public class CreateUeUseCase
         if (string.IsNullOrWhiteSpace(ue.NomUe) || ue.NomUe.Length <= 3)
             throw new InvalidUeNameException();
 
-        // Vérification de l’unicité du code
-        var existantes = await _ueRepository.FindByConditionAsync(u => u.Code == ue.Code);
-        if (existantes != null && existantes.Count > 0)
-            throw new DuplicateCodeUeException(ue.Code);
+        var repo = _factory.UeRepository();
 
+        // Vérification de l’unicité du code
+        var existantes = await repo.FindByConditionAsync(u => u.Code == ue.Code);
+        if (existantes.Any())
+            throw new DuplicateCodeUeException(ue.Code);
+        
         // Création si tout est bon
-        return await _ueRepository.CreateAsync(ue);
+        var result = await repo.CreateAsync(ue);
+
+        // Sauvegarde des modifications
+        await _factory.SaveChangesAsync();
+
+        return result;
     }
 }
